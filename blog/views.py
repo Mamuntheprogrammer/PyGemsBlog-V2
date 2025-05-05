92fe6259fd9724341bdf54bf73270c399d8ee268
Mamun <a.a.mamunbu@gmail.com>
2025-05-05 12:55:56 +0600

    Pagination exception handalled

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,22 @@ from .models import Post
 # from django.http import Http404
 
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 3)  # Show 3 posts per page
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day , post):
